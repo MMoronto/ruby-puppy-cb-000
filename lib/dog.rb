5612ad634834ad9b0aa122052de6d245966a72bb
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-04 03:54:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
   @@all = []
-  attr_accessor :name
+  attr_accessor :name :dog 
   
   def initialize(name)
-    self.name = name 
+    self.dog = name 
     @@all << self.name
     @name = name
   end 
